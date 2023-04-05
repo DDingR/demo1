6b1cbca3fb4b73068de87f9db3926474d34ed251
DDingR <msryu00@gmail.com>
2023-04-05 16:43:02 +0900

    04051642 pycarmaker edit (step control problem)

--- utils/env_manager.py
+++ utils/env_manager.py
@@ -7,13 +7,25 @@ class CM_dict():
         self.name = dictName
         self.type = dictType
         self.data = None
-        self.read_msg = f"expr {$Qu}"
+        self.read_msg = "expr {$Qu(" + self.name + ")}\r"
 
 class CM_Manager():
-    def __init__(self) -> None:
+    def __init__(self, CM_PATH, TCP_IP, TCP_PORT) -> None:
+        os.system(f"{CM_PATH} -cmdport {TCP_PORT} &")
+        time.system(2)
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((TCP_IP, TCP_PORT))
+    
+    def loadTestRun(self, testRun):
+        msg = "LoadTestRun %s" % (testRun)
+        self.send(msg)
+        time.sleep(500)
+
+    def startTestRun(self):
         pass
 
-    def connectCM(self):
+    def stopTestRun(self):
         pass
 
     def subscribe(self, quantity):
@@ -29,5 +41,9 @@ class CM_Manager():
         pass
 
     def send(self, msg):
-        pass
+        msg = msg + "\r" 
+        msg = msg.encode('utf-8')
+        self.socket.send(msg)
+        time.sleep(1)
+        return self.socket.recv(200)
 
